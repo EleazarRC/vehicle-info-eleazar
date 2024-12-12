3a6425334dd4983ca9b5fb16765cfba81b707e7c
Eleazar Ramos <sixen25@gmail.com>
2024-12-12 18:08:54 +0100

    cdk-virtual-scroll-viewport sin  mat-table

--- src/app/shared/table/table-component/table-component.component.ts
+++ src/app/shared/table/table-component/table-component.component.ts
@@ -1,16 +1,35 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { ScrollingModule, CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import {
+  ScrollingModule,
+  CdkVirtualScrollViewport,
+} from '@angular/cdk/scrolling';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 import { TableColumn } from '../../../models/table-column.model';
+import { CdkTableModule } from '@angular/cdk/table';
 
 @Component({
   selector: 'app-table-component',
   standalone: true,
-  imports: [CommonModule, ScrollingModule, MatButtonModule, MatIconModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    CommonModule,
+    ScrollingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+    CdkTableModule      
+  ],
   templateUrl: './table-component.component.html',
-  styleUrl: './table-component.component.scss'
+  styleUrl: './table-component.component.scss',
 })
 export class TableComponent {
   @Input() columns: TableColumn[] = [];
@@ -18,7 +37,17 @@ export class TableComponent {
   @Input() showActions: boolean = true;
   @Output() detailsClick = new EventEmitter<any>();
 
+  get displayedColumns(): string[] {
+    const colFields = this.columns.map((c) => c.field);
+    return this.showActions ? [...colFields, 'actions'] : colFields;
+  }
+
   onDetailsClick(row: any) {
     this.detailsClick.emit(row);
+  }
+
+  trackByFn(index: number, item: any) {
+    // Usa un ID único si existe, de lo contrario index
+    return item?.Make_ID ?? item?.VehicleTypeId ?? index;
   }
 }
